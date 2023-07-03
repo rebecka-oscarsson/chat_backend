c4b83f099f6faeace7817f481d2053345ccd7987
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-07-03 23:16:11 +0200

    bugfix directory not found

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 //app.use(express.static(__dirname + '/public'));
-//app.use("/images", express.static("./uploads"));
+//app.use("/uploads", express.static("./uploads"));
 app.use(express.static(__dirname));
 
 const avatarRouter = require("./routes/avatars");

8c303fe4af6b8921f191616631e2431604423565
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-07-04 15:40:23 +0200

    -

--- multer.js
+++ multer.js
@@ -1,8 +1,11 @@
 const multer = require("multer");
 const path = require('path');
+const fs = require("fs");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '/uploads'));
+    const uploadsfolder = path.join(__dirname, '/uploads');
+    fs.mkdirSync(uploadsfolder, { recursive: true })
+    cb(null, uploadsfolder);
   },
   filename: function (req, file, cb) {
     const filename = `${Date.now()}-${file.originalname}`;
